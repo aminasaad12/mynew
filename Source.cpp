ef9cdcea0119931ff55ce5d4c05c4d2275a23d0c
Amina Yehia <aminasaad1218@gmail.com>
2020-12-20 08:27:06 +0200

    ecommit

---
+++ Source.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	const  int size=5;
+	int array[size];
+	int sum=0p
+	for(int i=0 ;i< size ;i++)
+	{
+		cout<<"please entet the elment of array "<<i+1<<endl;
+		cin>>array[i];
+		
+	}
+		for(int i=0 ;i< size ;i++)
+		{
+			cout<<"element of array "<<i+1<<"="<<array[i]<<endl;
+		}
+
+
+
+
+
+
+
+	system("pause");
+}
